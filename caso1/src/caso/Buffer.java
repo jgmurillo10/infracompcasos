b5c571056b84dc8c82ace485392bb68d227eaaf6
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2017-03-05 17:00:51 -0500

    base project setup

---
+++ caso1/src/caso/Buffer.java
@@ -0,0 +1,64 @@
+package caso;
+
+import java.util.ArrayList;
+
+public class Buffer {
+	private int tam;
+
+	private int numClientes;
+
+	private boolean termino;
+
+	private ArrayList<Mensaje> mensajes;
+
+	public Buffer(int tam, int numClientes) {
+		this.tam = tam;
+		this.numClientes = numClientes;
+		this.termino = false;
+		this.mensajes = new ArrayList<>();
+	}
+
+	public synchronized boolean enviar(Mensaje m) {
+		boolean logroEnviar = false;
+		if (tam <= mensajes.size()) {
+			notify();
+		} else {
+			mensajes.add(m);
+			System.out.println("Buffer: Se añadio el mensaje " + m.getValor() + ". Ahora hay " + mensajes.size() + " mensajes.");
+			logroEnviar = true;
+			notify();
+		}
+		return logroEnviar;
+	}
+
+	public synchronized void terminoMensajes() {
+		numClientes--;
+		if (numClientes == 0) {
+			termino = true;
+			notifyAll();
+		}
+		notifyAll();
+	}
+
+	public synchronized void retirar() {
+		while (0 == mensajes.size() && !termino) {
+			try {
+				wait();
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
+		if (!termino) {
+			Mensaje m = mensajes.remove(0);
+			m.leer();
+		}
+	}
+
+	public boolean termino() {
+		return termino;
+	}
+
+	public void setTermino(boolean termino) {
+		this.termino = termino;
+	}
+}
---
+++ caso1/src/caso/Mensaje.java
@@ -0,0 +1,32 @@
+package caso;
+
+public class Mensaje {
+	
+	private int valor;
+	
+	public Mensaje (int valor) {
+		this.valor = valor;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	public synchronized void esperar() {
+		try {
+			wait();
+		} catch(InterruptedException ie) {
+			ie.printStackTrace();
+		}
+	}
+	
+	public synchronized void leer() {
+		valor++;
+		System.out.println("Mensaje: Avisando que cambie valor " + valor);
+		notify();
+	}
+}
